831d43120b4a060b6d2ab34c9df1d7c99b6754b3
SamuelFoc <samo.sipikal@gmail.com>
2022-11-20 18:47:33 +0100

    Add Database

--- server/app/config/allowedOrigins.js
+++ server/app/config/allowedOrigins.js
@@ -1,9 +1,11 @@
 // TODO: CORS (Cross-Origin-Resource-Sharing) these are the alowed origins that can send requests to this API.
 
 const allowedOrigins = [
-  "https://www.mywebsite.com",
+  "https://www.aha-cloud.com",
   "http://localhost",
   "http://localhost:3000",
+  "192.168.0.2",
+  "192.168.0.2:3000",
 ];
 
 module.exports = allowedOrigins;
